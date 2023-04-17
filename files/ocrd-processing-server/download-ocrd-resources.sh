1e7ea585b73e6689950d3328e8bb7133fc7fc8eb
joschrew <jonas.schrewe@gwdg.de>
2023-04-17 16:09:45 +0200

    Change ocrd-tesserocr-recognize resource download

--- files/ocrd-processing-server/download-ocrd-resources.sh
+++ files/ocrd-processing-server/download-ocrd-resources.sh
@@ -13,6 +13,9 @@ mkdir -p $DEST
 # Download ocrd resources
 docker run --rm -v "$DEST:/usr/local/share/ocrd-resources" -- ocrd/all:maximum ocrd resmgr download -a '*'
 
-# Download tesserocr-recognize resources (apperently isn't covered/downloaded with previous call)
-docker run --rm -v "$DEST:/usr/local/share/ocrd-resources" -- ocrd/all:maximum ocrd resmgr download -a '*'
-docker run --rm -v "$DEST/ocrd-tesserocr-recognize:/usr/local/share/tessdata" -- ocrd/all:maximum ocrd resmgr download ocrd-tesserocr-recognize '*'
+# Download tesserocr-recognize resources
+sudo -s <<EOF
+. /home/cloud/venv-ocrd/bin/activate
+ocrd resmgr download ocrd-tesserocr-recognize '*'
+deactivate
+EOF
