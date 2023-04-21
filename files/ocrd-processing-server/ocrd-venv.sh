ceb4e2e96552544a15719be6063aec60598a8027
joschrew <jonas.schrewe@gwdg.de>
2023-04-21 13:09:02 +0200

    Update and reorganize venv-script

--- files/ocrd-processing-server/ocrd-venv.sh
+++ files/ocrd-processing-server/ocrd-venv.sh
@@ -7,11 +7,12 @@
 
 DEPS="python3.7 python3.7-venv python3.7-dev make parallel git wget imagemagick libgeos-dev "
 DEPS+="libtesseract-dev libleptonica-dev tesseract-ocr-eng tesseract-ocr-script-frak tesseract-ocr "
+DEPS+="automake xmlstarlet ca-certificates libmagick++-6.q16-dev libgraphicsmagick++1-dev libboost-dev"
 VENV="$HOME/venv-ocrd"
 REPOS="$HOME/repos"
+
 # update processor repositories (maybe later this can be used as a script parameter)
 UPDATE=0
-
 
 # read cmd opts
 while getopts ":u" param; do
@@ -21,37 +22,41 @@ while getopts ":u" param; do
 done
 shift $((OPTIND -1))
 
-
-# add deadsnakes ppa to install python3.7
-add_deadsnakes=0
-find /etc/apt/ -name *.list | xargs cat | grep  "^[[:space:]]*deb" | grep "deadsnakes/ppa" -q || add_deadsnakes=1
-if test $add_deadsnakes -ne 0; then
-	sudo add-apt-repository -y ppa:deadsnakes/ppa
-fi
-# add alex-p ppa to install tesseract-stuff
-add_alexp=0
-find /etc/apt/ -name *.list | xargs cat | grep  "^[[:space:]]*deb" | grep "alex-p/tesseract-ocr" -q || add_alexp=1
-if test $add_alexp -ne 0; then
-	sudo add-apt-repository -y ppa:alex-p/tesseract-ocr
-fi
-
-# Install requierements(DEPS)
-for package in $DEPS; do
-	is_installed=1
-	dpkg -s $package || is_installed=0
-	if test $is_installed -eq 0; then
-		sudo apt install -y $package
+# Install all dependencies for OCR-D with apt
+function install_dependencies() {
+	# add deadsnakes ppa to install python3.7
+	add_deadsnakes=0
+	find /etc/apt/ -name *.list | xargs cat | grep  "^[[:space:]]*deb" | grep "deadsnakes/ppa" -q || add_deadsnakes=1
+	if test $add_deadsnakes -ne 0; then
+		sudo add-apt-repository -y ppa:deadsnakes/ppa
 	fi
-done
+	# add alex-p ppa to install tesseract-stuff
+	add_alexp=0
+	find /etc/apt/ -name *.list | xargs cat | grep  "^[[:space:]]*deb" | grep "alex-p/tesseract-ocr" -q || add_alexp=1
+	if test $add_alexp -ne 0; then
+		sudo add-apt-repository -y ppa:alex-p/tesseract-ocr
+	fi
 
-# create venv if not existing
-if ! test -d $VENV; then
-	python3.7 -m venv $VENV
-fi
+	# Install requierements(DEPS)
+	for package in $DEPS; do
+		is_installed=1
+		dpkg -s $package || is_installed=0
+		if test $is_installed -eq 0; then
+			sudo apt install -y $package
+		fi
+	done
+}
 
-mkdir -p $REPOS
+# Create venv if not existing
+function create_venv() {
+	if ! test -d $VENV; then
+		python3.7 -m venv $VENV
+	fi
 
-# clone and optional update github repo
+	mkdir -p $REPOS
+}
+
+# Clone and optional update github repo
 #
 # param1: destination dir-name of repo
 # param2: git-clone-url
@@ -82,59 +87,80 @@ function pull_package {
 	return 0
 }
 
-# clone processors
-update_core=0
-pull_package ocrd_core https://github.com/OCR-D/core.git $UPDATE dev-processing-broker || update_core=$?
+# Install procesors: Clone / update core and all processors and then (re)install
+function install_processors() {
+	# clone processors
+	update_core=0
+	pull_package ocrd_core https://github.com/OCR-D/core.git $UPDATE dev-processing-broker || update_core=$?
 
-# this script has set -e so fails on errors. this `pull_package ... || update_x=$?` is to make
-# return nonzero possible. pipefail is of so only last exit code (when using pipe or `||`) has to be
-# zero
-update_cis=0
-pull_package ocrd_cis https://github.com/cisocrgroup/ocrd_cis $UPDATE || update_cis=$?
-update_anybaseocr=0
-pull_package ocrd_anybaseocr https://github.com/OCR-D/ocrd_anybaseocr.git $UPDATE || update_anybaseocr=$?
-update_wrap=0
-pull_package ocrd_wrap https://github.com/bertsky/ocrd_wrap.git $UPDATE || update_wrap=$?
-update_tesserocr=0
-pull_package ocrd_tesserocr https://github.com/OCR-D/ocrd_tesserocr.git $UPDATE || update_tesserocr=$?
-update_calamari=0
-pull_package ocrd_calamari https://github.com/OCR-D/ocrd_calamari.git $UPDATE || update_calamari=$?
-update_fileformat=0
-pull_package ocrd_fileformat https://github.com/OCR-D/ocrd_fileformat.git $UPDATE || update_fileformat=$?
+	# this script has set -e so fails on errors. this `pull_package ... || update_x=$?` is to make
+	# return nonzero possible. pipefail is of so only last exit code (when using pipe or `||`) has to be
+	# zero
+	update_cis=0
+	pull_package ocrd_cis https://github.com/cisocrgroup/ocrd_cis $UPDATE || update_cis=$?
+	update_anybaseocr=0
+	pull_package ocrd_anybaseocr https://github.com/OCR-D/ocrd_anybaseocr.git $UPDATE || update_anybaseocr=$?
+	update_wrap=0
+	pull_package ocrd_wrap https://github.com/bertsky/ocrd_wrap.git $UPDATE || update_wrap=$?
+	update_tesserocr=0
+	pull_package ocrd_tesserocr https://github.com/OCR-D/ocrd_tesserocr.git $UPDATE || update_tesserocr=$?
+	update_calamari=0
+	pull_package ocrd_calamari https://github.com/OCR-D/ocrd_calamari.git $UPDATE || update_calamari=$?
+	update_fileformat=0
+	pull_package ocrd_fileformat https://github.com/OCR-D/ocrd_fileformat.git $UPDATE || update_fileformat=$?
+	update_olena=0
+	pull_package ocrd_olena https://github.com/OCR-D/ocrd_olena.git $UPDATE || update_olena=$?
+	update_segment=0
+	pull_package ocrd_segment https://github.com/OCR-D/ocrd_segment.git $UPDATE || update_segment=$?
 
-# install processors
-. $VENV/bin/activate
+	# install processors
+	# for updating the installation is simply run again, maybe that is not ideal?
+	. $VENV/bin/activate
 
-cd $REPOS/ocrd_core
-if ! test -f $VENV/bin/ocrd || test $update_core -ne 0; then
-	make install-dev  # TODO: change for after development phase?!
-fi
+	cd $REPOS/ocrd_core
+	if ! test -f $VENV/bin/ocrd || test $update_core -ne 0; then
+		make install-dev  # TODO: change for after development phase?!
+	fi
 
-cd $REPOS/ocrd_cis
-if ! test -f $VENV/bin/ocrd-cis-ocropy-binarize || test $update_cis -ne 0; then
-	make install
-fi
-cd $REPOS/ocrd_anybaseocr
-if ! test -f $VENV/bin/ocrd-anybaseocr-crop || test $update_anybaseocr -ne 0; then
-	make install
-fi
-cd $REPOS/ocrd_wrap
-if ! test -f $VENV/bin/ocrd-skimage-binarize || test $update_wrap -ne 0; then
-	make install
-fi
-cd $REPOS/ocrd_tesserocr
-if ! test -f $VENV/bin/ocrd-tesserocr-deskew || test $update_tesserocr -ne 0; then
-	make install
-fi
-cd $REPOS/ocrd_calamari
-if ! test -f $VENV/bin/ocrd-calamari-recognize || test $update_calamari -ne 0; then
-	make install
-fi
-cd $REPOS/ocrd_fileformat
-if ! test -f $VENV/bin/ocrd-fileformat-transform || test $update_fileformat -ne 0; then
-	git submodule update --init --recursive
-	# make install reinstalls ocrd-core which would be redundant
-	make install-fileformat install-tools
-fi
+	cd $REPOS/ocrd_cis
+	if ! test -f $VENV/bin/ocrd-cis-ocropy-binarize || test $update_cis -ne 0; then
+		make install
+	fi
+	cd $REPOS/ocrd_anybaseocr
+	if ! test -f $VENV/bin/ocrd-anybaseocr-crop || test $update_anybaseocr -ne 0; then
+		make install
+	fi
+	cd $REPOS/ocrd_wrap
+	if ! test -f $VENV/bin/ocrd-skimage-binarize || test $update_wrap -ne 0; then
+		make install
+	fi
+	cd $REPOS/ocrd_tesserocr
+	if ! test -f $VENV/bin/ocrd-tesserocr-deskew || test $update_tesserocr -ne 0; then
+		make install
+	fi
+	cd $REPOS/ocrd_calamari
+	if ! test -f $VENV/bin/ocrd-calamari-recognize || test $update_calamari -ne 0; then
+		make install
+	fi
+	cd $REPOS/ocrd_fileformat
+	if ! test -f $VENV/bin/ocrd-fileformat-transform || test $update_fileformat -ne 0; then
+		git submodule update --init --recursive
+		# make install reinstalls ocrd-core which would be redundant
+		make install-fileformat install-tools
+	fi
+	cd $REPOS/ocrd_olena
+	if ! test -f $VENV/bin/ocrd-olena-binarize || test $update_olena -ne 0; then
+		make install
+	fi
+	cd $REPOS/ocrd_segment
+	if ! test -f $VENV/bin/ocrd-segment-repair || test $update_segment -ne 0; then
+		pip install .
+	fi
 
-deactivate
+	deactivate
+}
+
+
+install_dependencies
+create_venv
+install_processors
