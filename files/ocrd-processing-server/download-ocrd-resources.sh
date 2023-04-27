3fccb6d86de0343b439c14c4de118b9b0223cb89
joschrew <jonas.schrewe@gwdg.de>
2023-04-27 10:20:50 +0200

    Use ocrd_all for installing venv

--- files/ocrd-processing-server/download-ocrd-resources.sh
+++ files/ocrd-processing-server/download-ocrd-resources.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/bash -e
 #
-# This downloads the ocrd-resources. A container is used so that all processors independend from
-# local setup will be downloaded.
-#
-# Updates are not planned, they should be done with `ocrd resmgr` on the vm itself. Purpose of this
-# script is only to make the initial download of models
+# ocrd-tesserocr-recognize is called seperatly (in addition to download '*'), otherwise models
+# are not put into venv/share/tessdata
 
-DEST="/home/cloud/.local/share/ocrd-resources"
-
-mkdir -p $DEST
-
-# Download ocrd resources
-docker run --rm -v "$DEST:/usr/local/share/ocrd-resources" -- ocrd/all:maximum ocrd resmgr download -a '*'
-
-# Download tesserocr-recognize resources
-sudo -s <<EOF
-. /home/cloud/venv-ocrd/bin/activate
+. /home/cloud/repos/ocrd_all/venv/bin/activate
+ocrd resmgr download '*'
 ocrd resmgr download ocrd-tesserocr-recognize '*'
 deactivate
-EOF
